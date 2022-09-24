53c49fe08223270d16e16359cc3e800f921b7bda
Edouard <edouard@1000words-hq.com>
2022-09-24 11:11:28 -0700

    Bug fix

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import streamlit as st
 
-option = st.selectbox('How would you like to be contacted?', ('Email', 'Home phone', 'Mobile phone'))
-st.experimental_set_query_params(
-  option=option,
-)
+options = ['Email', 'Home phone', 'Mobile phone']
 
 params = st.experimental_get_query_params()
-option = params.get("option", ["None"])[0]
+option = params.get("option", ["Email"])[0]
+
+def navigate():
+  st.experimental_set_query_params(
+    option=st.session_state.selected_option,
+  )
+
+st.selectbox(
+  'How would you like to be contacted?', 
+  options,
+  index=options.index(option),
+  on_change=navigate,
+  key="selected_option"
+)
+
 st.write(f"You selected {option}")
